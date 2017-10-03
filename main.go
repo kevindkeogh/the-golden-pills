95e912a06e6eff95e2dd1af1f6bc95dea054644e
Kevin Keogh <kevin.d.keogh@gmail.com>
2017-10-02 23:35:09 -0400

    Too many non-GG gifs in the top 500, trying 400. Also added docstrings

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/paddycarey/gophy"
 )
 
-//
+// giphyLogin returns the Giphy connected API Client
 func giphyLogin() *gophy.Client {
 	giphyOptions := &gophy.ClientOptions{
 		ApiKey: giphyApiKey}
@@ -24,9 +24,12 @@ func giphyLogin() *gophy.Client {
 	return client
 }
 
+// gifString takes the Giphy API Client and returns a base 64 encoded string
+// of a random Golden Girls gif. Note that the function searches the term
+// "golden girls" and returns a random result from the top 400 results.
 func gifString(client *gophy.Client) string {
 	// Get gif object from giphy
-	randOffset := rand.Intn(500)
+	randOffset := rand.Intn(400)
 	gifs, _, err := client.SearchGifs("golden girls", "", 1, randOffset)
 	if err != nil {
 		fmt.Println(err)
@@ -47,7 +50,7 @@ func gifString(client *gophy.Client) string {
 	return encoded
 }
 
-//
+// twitterLogin returns the Twitter connected API Client
 func twitterLogin() *anaconda.TwitterApi {
 	anaconda.SetConsumerKey(twitterConsumerKey)
 	anaconda.SetConsumerSecret(twitterConsumerSecret)
@@ -55,7 +58,8 @@ func twitterLogin() *anaconda.TwitterApi {
 	return api
 }
 
-//
+// sendTweet takes the Twitter API client, the text of the tweet to be sent,
+// and the ID of the previously uploaded GIF..
 func sendTweet(client *anaconda.TwitterApi, text string, mediaID string) {
 	v := url.Values{}
 	v.Set("media_ids", mediaID)
@@ -65,6 +69,7 @@ func sendTweet(client *anaconda.TwitterApi, text string, mediaID string) {
 	}
 }
 
+// redditLogin returns the Reddit connected API Client
 func redditLogin() *geddit.OAuthSession {
 	client, err := geddit.NewOAuthSession(
 		redditID,
@@ -82,7 +87,10 @@ func redditLogin() *geddit.OAuthSession {
 	return client
 }
 
-//
+// redPillComment takes a Reddit API Client and returns the string of a random
+// recent comment. Note that the string is stripped to ensure that it is <140
+// characters. It is also HTML unescaped, and included the /u/{username}
+// signature of the commenter
 func redPillComment(client *geddit.OAuthSession) string {
 	comments, err := client.SubredditComments("TheRedPill")
 	if err != nil {
@@ -99,12 +107,12 @@ func redPillComment(client *geddit.OAuthSession) string {
 		num := rand.Intn(numComments)
 		comment = comments[num].Body
 		author = comments[num].Author
-		text = html.UnescapeString(comment + "&#13;&#10;&#13;&#10; /u/" + author)
+		text = html.UnescapeString(comment + "&#13;&#10;&#13;&#10;/u/" + author)
 		for len(text) > 140 && numGuesses < 10 {
 			numGuesses++
 			splitComment := strings.Split(comment, ".")
 			comment = strings.Join(splitComment[:len(splitComment)-1], ".") + "."
-			text = html.UnescapeString(comment + "&#13;&#10;&#13;&#10; /u/" + author)
+			text = html.UnescapeString(comment + "&#13;&#10;&#13;&#10;/u/" + author)
 		}
 		if len(text) <= 140 && len(comment) > 1 {
 			break
@@ -113,7 +121,8 @@ func redPillComment(client *geddit.OAuthSession) string {
 	return text
 }
 
-//
+// makePost creates new API clients for Giphy, Twitter, and Reddit, and creates
+// a single tweet
 func makePost() {
 	giphyClient := giphyLogin()
 	twitterClient := twitterLogin()
@@ -126,11 +135,11 @@ func makePost() {
 	sendTweet(twitterClient, text, media.MediaIDString)
 }
 
-//
+// main runs forever and executes the makePost function every 30 minutes
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	for {
 		makePost()
-		time.Sleep(20 * time.Minute)
+		time.Sleep(30 * time.Minute)
 	}
 }
